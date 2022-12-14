be5b1ea1b7c1d0b4c00ae7ca7f5db27f060d6ba1
Duy Do <duydo@protonmail.com>
2022-12-15 00:32:57 +0700

    Make it less painful

--- node-definitions/WASM/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# rust
-for NODE in $(ls | grep -v .sh); do
-    echo "Building $NODE"
-    $(cd $NODE && cargo build --release --target wasm32-wasi && cp target/wasm32-wasi/release/$NODE.wasm ../$NODE)
-done
---
+++ wasm/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cargo build --release --target wasm32-wasi
+
+for NODE in ./cmds/*; do
+    cp -v ./target/wasm32-wasi/release/${NODE##*/}.wasm $NODE/
+done
