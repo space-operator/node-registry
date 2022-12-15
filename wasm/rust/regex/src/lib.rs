ace2ea2fe1853532f50aad9ae6bfced1b7632001
enzotar <34943955+enzotar@users.noreply.github.com>
2022-12-15 09:51:57 -0800

    @zheland regex updates (#20)

--- wasm/rust/regex/src/lib.rs
+++ wasm/rust/regex/src/lib.rs
@@ -1,65 +1,138 @@
-use regex::Regex;
-use serde::Serialize;
-use serde_json::Value;
+use std::collections::HashMap;
+use std::fmt::Display;
+
+use regex::{Error as RegexError, Regex};
+use serde::{Serialize, Serializer};
+
+#[derive(Serialize)]
+struct Captures {
+    r#match: String,
+    unnamed: Vec<Option<String>>,
+    named: HashMap<String, Option<String>>,
+}
+
+#[cfg(not(test))]
+#[no_mangle]
+fn main(text: &String, regex: &String) -> Box<String> {
+    iter_captures(text, regex)
+}
+
+fn iter_captures(text: &String, regex: &String) -> Box<String> {
+    serialize_result(iter_captures_impl(text, regex))
+}
+
+fn iter_captures_impl(text: &String, regex: &String) -> Result<Vec<Captures>, RegexError> {
+    let re = Regex::new(regex)?;
+    let names: Vec<_> = re.capture_names().flatten().collect();
+    let len = re.captures_len();
+    Ok(re
+        .captures_iter(text)
+        .map(|caps| Captures {
+            r#match: caps.get(0).unwrap().as_str().to_owned(),
+            unnamed: (1..len)
+                .map(|j| caps.get(j).map(|item| item.as_str().to_owned()))
+                .collect(),
+            named: names
+                .iter()
+                .map(|&name| {
+                    (
+                        name.to_owned(),
+                        caps.name(name).map(|item| item.as_str().to_owned()),
+                    )
+                })
+                .collect(),
+        })
+        .collect())
+}
 
 #[derive(Serialize)]
 #[serde(tag = "status", rename_all = "lowercase")]
-enum IsMatchResult {
-    Ok { value: bool },
-    Error { error: String },
+#[serde(bound(serialize = "T: Serialize, E: Display"))]
+enum ResultSer<T, E> {
+    Ok {
+        value: T,
+    },
+    Error {
+        #[serde(serialize_with = "use_display")]
+        error: E,
+    },
 }
 
-#[no_mangle]
-fn is_match(text: &String, regex: &String) -> Box<String> {
-    fn merge(a: &mut Value, b: Value) {
-        match (a, b) {
-            (a @ &mut Value::Object(_), Value::Object(b)) => {
-                let a = a.as_object_mut().unwrap();
-                for (k, v) in b {
-                    merge(a.entry(k).or_insert(Value::Null), v);
-                }
-            }
-            (a, b) => *a = b,
-        }
-    }
-
-    let re = Regex::new(regex);
-
-    let is_match_result = match &re {
-        Ok(re) => IsMatchResult::Ok {
-            value: re.is_match(text),
-        },
-        Err(err) => IsMatchResult::Error {
-            error: err.to_string(),
-        },
+fn serialize_result<T, E>(result: Result<T, E>) -> Box<String>
+where
+    T: Serialize,
+    E: Display,
+{
+    let result = match result {
+        Ok(value) => ResultSer::Ok { value },
+        Err(error) => ResultSer::Error { error },
     };
-    let _is_match = serde_json::to_string(&is_match_result).unwrap();
+    Box::new(serde_json::to_string(&result).unwrap())
+}
 
-    let capture_group = re
-        .unwrap()
-        .find_iter(text)
-        .map(|m| m.as_str())
-        .collect::<Vec<_>>();
-
-    let mut captures = serde_json::to_value(&capture_group).unwrap();
-    let res = merge(
-        &mut captures,
-        serde_json::to_value(&is_match_result).unwrap(),
-    );
-    dbg!(res);
-    Box::new(serde_json::to_string(&is_match_result).unwrap())
+fn use_display<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: Display,
+    S: Serializer,
+{
+    serializer.collect_str(value)
 }
 
 #[cfg(test)]
 mod tests {
-    fn is_match(text: &str, regex: &str) -> String {
-        *super::is_match(&text.to_owned(), &regex.to_owned())
-    }
+    use super::iter_captures;
 
     #[test]
     fn regex() {
-        assert_eq!(is_match("this is some test", r"[a-z]+"), "123");
-        // assert_eq!(is_match("abcd", "e"), "{\"status\":\"ok\",\"value\":false}");
-        // assert_eq!(is_match("abcd", "\\"), "{\"status\":\"error\",\"error\":\"regex parse error:\\n    \\\\\\n    ^\\nerror: incomplete escape sequence, reached end of pattern prematurely\"}");
+        fn check(text: &str, regex: &str) -> String {
+            *iter_captures(&text.to_owned(), &regex.to_owned())
+        }
+
+        assert_eq!(
+            check("abcd", "a"),
+            "{\
+                \"status\":\"ok\",\
+                \"value\":[{\"match\":\"a\",\"unnamed\":[],\"named\":{}}]\
+            }"
+        );
+        assert_eq!(
+            check("abcd", "e"),
+            "{\
+                \"status\":\"ok\",\
+                \"value\":[]\
+            }"
+        );
+        assert_eq!(
+            check("abcd", "\\"),
+            "{\
+                \"status\":\"error\",\
+                \"error\":\"regex parse error:\\n    \\\\\\n    ^\\n\
+                    error: incomplete escape sequence, reached end of pattern prematurely\"\
+            }"
+        );
+        assert_eq!(
+            check(
+                "@ a 123 @ b !bar 234 @ a foo 345",
+                "@ ([a-z]+)(?: (?P<NamedGroup>[a-z]+))?(?: !([a-z]+))? ([0-9]+)"
+            ),
+            "{\
+                \"status\":\"ok\",\
+                \"value\":[\
+                    {\
+                        \"match\":\"@ a 123\",\
+                        \"unnamed\":[\"a\",null,null,\"123\"],\
+                        \"named\":{\"NamedGroup\":null}\
+                    },{\
+                        \"match\":\"@ b !bar 234\",\
+                        \"unnamed\":[\"b\",null,\"bar\",\"234\"],\
+                        \"named\":{\"NamedGroup\":null}\
+                    },{\
+                        \"match\":\"@ a foo 345\",\
+                        \"unnamed\":[\"a\",\"foo\",null,\"345\"],\
+                        \"named\":{\"NamedGroup\":\"foo\"}\
+                    }\
+                ]\
+            }"
+        );
     }
 }
