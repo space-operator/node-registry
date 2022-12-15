5945c54a1f5d418bfa37dd49b4ded5b7d93efc76
Kival Mahadew <kivalm@protonmail.com>
2022-12-15 23:52:10 +0200

    add basic snake and camel case conversion

---
+++ wasm/rust/camel_to_snake_case/src/lib.rs
@@ -0,0 +1,19 @@
+use convert_case::{Case, Casing};
+
+#[no_mangle]
+fn main(input: &String) -> Box<String> {
+    let result = input.to_case(Case::Snake);
+    Box::new(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_camel_to_snake_case() {
+        let input = "camelCase".to_string();
+        let result = *main(&input);
+        assert_eq!(*result, "camel_case".to_string());
+    }
+}
