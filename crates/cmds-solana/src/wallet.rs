c32069d6e4f682c51687e2610e843a4092d1d963
Duy Do <duydo@protonmail.com>
2022-12-15 16:04:41 +0700

    Update wallet command

--- crates/cmds-solana/src/wallet.rs
+++ crates/cmds-solana/src/wallet.rs
@@ -1,7 +1,70 @@
 use crate::prelude::*;
+use flow_lib::config::client::NodeData;
+use thiserror::Error as ThisError;
 
 #[derive(Debug)]
-pub struct Wallet;
+pub struct Wallet {
+    form: Option<Result<Output, WalletError>>,
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "wallet_type")]
+enum FormData {
+    #[serde(rename = "HARDCODED")]
+    HardCoded { wallet_data: String },
+    #[serde(rename = "ADAPTER")]
+    Adapter { wallet_data: String },
+}
+
+#[derive(ThisError, Debug, Clone)]
+enum WalletError {
+    #[error("failed to decode wallet as base58")]
+    InvalidBase58,
+}
+
+fn adapter_wallet(pubkey: Pubkey) -> Output {
+    let mut buf = [0u8; 64];
+    buf[32..].copy_from_slice(&pubkey.to_bytes());
+    let keypair = Keypair::from_bytes(&buf).expect("correct size, never fail");
+    Output { pubkey, keypair }
+}
+
+impl FormData {
+    fn to_output(self) -> Result<Output, WalletError> {
+        match self {
+            FormData::Adapter { wallet_data } => {
+                let pubkey = wallet_data
+                    .parse::<Pubkey>()
+                    .map_err(|_| WalletError::InvalidBase58)?;
+                Ok(adapter_wallet(pubkey))
+            }
+            FormData::HardCoded { wallet_data } => {
+                let mut buf = [0u8; 64];
+                let size = bs58::decode(wallet_data.trim())
+                    .into(&mut buf)
+                    .map_err(|_| WalletError::InvalidBase58)?;
+                if size != buf.len() {
+                    return Err(WalletError::InvalidBase58);
+                }
+                let keypair = Keypair::from_bytes(&buf).expect("correct size, never fail");
+
+                Ok(Output {
+                    pubkey: keypair.pubkey(),
+                    keypair,
+                })
+            }
+        }
+    }
+}
+
+impl Wallet {
+    fn new(nd: &NodeData) -> Self {
+        let form = serde_json::from_value::<FormData>(nd.targets_form.form_data.clone())
+            .ok()
+            .map(FormData::to_output);
+        Self { form }
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Output {
@@ -38,33 +101,88 @@ impl CommandTrait for Wallet {
     }
 
     async fn run(&self, ctx: Context, _inputs: ValueSet) -> Result<ValueSet, CommandError> {
-        let pubkey = ctx.user.pubkey;
-        let mut keypair = [0u8; 64];
-        keypair[32..].copy_from_slice(&pubkey.to_bytes());
-        let keypair = Keypair::from_bytes(&keypair).map_err(crate::Error::custom)?;
-
-        Ok(value::to_map(&Output { pubkey, keypair })?)
+        match &self.form {
+            None => Ok(value::to_map(&adapter_wallet(ctx.user.pubkey))?),
+            Some(result) => match result {
+                Ok(output) => Ok(value::to_map(output)?),
+                Err(e) => Err(e.clone().into()),
+            },
+        }
     }
 }
 
-inventory::submit!(CommandDescription::new(WALLET, |_| { Box::new(Wallet) }));
+inventory::submit!(CommandDescription::new(WALLET, |nd| {
+    Box::new(Wallet::new(nd))
+}));
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use flow_lib::config::client::{Extra, TargetsForm};
+    use serde_json::json;
+
+    const PUBKEY: &str = "DKsvmM9hfNm4R94yB3VdYMZJk2ETv5hpcjuRmiwgiztY";
 
     #[tokio::test]
     async fn test_wallet() {
-        let pubkey: Pubkey = "DKsvmM9hfNm4R94yB3VdYMZJk2ETv5hpcjuRmiwgiztY"
-            .parse()
-            .unwrap();
+        let pubkey: Pubkey = PUBKEY.parse().unwrap();
 
         let mut ctx = Context::default();
         ctx.user.pubkey = pubkey;
 
-        let result = Wallet.run(ctx, Default::default()).await.unwrap();
+        let result = Wallet { form: None }
+            .run(ctx, Default::default())
+            .await
+            .unwrap();
         let output: Output = value::from_map(result).unwrap();
         assert_eq!(output.pubkey, pubkey);
         assert_eq!(output.keypair.pubkey(), pubkey);
+    }
+
+    #[test]
+    fn adapter() {
+        let nd = NodeData {
+            r#type: flow_lib::CommandType::Native,
+            node_id: WALLET.into(),
+            sources: Vec::new(),
+            targets: Vec::new(),
+            targets_form: TargetsForm {
+                form_data: json!({
+                    // there is also "wallet_id", but it is not used
+                    "wallet_type": "ADAPTER",
+                    "wallet_data": PUBKEY,
+                }),
+                extra: Extra::default(),
+                wasm_bytes: None,
+            },
+        };
+        assert_eq!(
+            Wallet::new(&nd).form.unwrap().unwrap().pubkey.to_string(),
+            PUBKEY
+        );
+    }
+
+    #[test]
+    fn hardcoded() {
+        const KEYPAIR: &str = "oLXLpXdGn6RjMHz3fvcPdGNUDQxXu91t7YAFbtRew3TFVPHAU1UrZJpgiHDLKDtrWZRQg6trQFFp6zEX2TQ1S3k";
+
+        let nd = NodeData {
+            r#type: flow_lib::CommandType::Native,
+            node_id: WALLET.into(),
+            sources: Vec::new(),
+            targets: Vec::new(),
+            targets_form: TargetsForm {
+                form_data: json!({
+                    // there is also "wallet_id", but it is not used
+                    "wallet_type": "HARDCODED",
+                    "wallet_data": KEYPAIR,
+                }),
+                extra: Extra::default(),
+                wasm_bytes: None,
+            },
+        };
+        let wallet = Wallet::new(&nd).form.unwrap().unwrap();
+        assert_eq!(wallet.keypair.to_base58_string(), KEYPAIR,);
+        assert_eq!(wallet.keypair.pubkey(), wallet.pubkey);
     }
 }
