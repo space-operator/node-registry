bcddc7c98a889bb685dd4e83e61ee0b497f2d0fa
Duy Do <duydo@protonmail.com>
2023-01-19 15:03:55 +0700

    Revert "Proxy authority (#38)" (#39)
    This reverts commit ff37ccdddf6c69806bb2024f397ce3c2ae88cf7e.

--- crates/cmds-solana/src/nft/create_master_edition.rs
+++ crates/cmds-solana/src/nft/create_master_edition.rs
@@ -1,57 +1,8 @@
-use std::str::FromStr;
-
 use crate::prelude::*;
-use anchor_lang::Discriminator;
-use borsh::BorshSerialize;
-use solana_program::{
-    instruction::{AccountMeta, Instruction},
-    system_program,
-};
-
-use space_wrapper::instruction::ProxyCreateMasterEditionV3 as Proxy;
+use solana_program::instruction::Instruction;
 
 #[derive(Debug, Clone)]
 pub struct CreateMasterEdition;
-
-pub fn create_proxy_create_master_edition_instruction(
-    authority: &Pubkey,
-    proxy_authority: &Pubkey,
-    edition: &Pubkey,
-    mint: &Pubkey,
-    mint_authority: &Pubkey,
-    metadata: &Pubkey,
-    token_metadata_program: &Pubkey,
-    token_program: &Pubkey,
-    max_supply: u64,
-) -> Instruction {
-    let accounts = vec![
-        AccountMeta::new_readonly(*authority, true),
-        AccountMeta::new(*proxy_authority, false),
-        AccountMeta::new(*edition, false),
-        AccountMeta::new(*mint, false),
-        AccountMeta::new_readonly(*mint_authority, true),
-        AccountMeta::new(*metadata, false),
-        AccountMeta::new_readonly(*token_metadata_program, false),
-        AccountMeta::new_readonly(*token_program, false),
-        AccountMeta::new_readonly(system_program::id(), false),
-    ];
-
-    let mut data = vec![max_supply.to_le_bytes().to_vec()];
-
-    let proxy = Proxy {
-        max_supply: Some(max_supply),
-    };
-
-    let mut instruction_data: Vec<u8> = Proxy::discriminator().try_to_vec().unwrap();
-    instruction_data.append(BorshSerialize::try_to_vec(&proxy).unwrap().as_mut());
-    data.insert(0, instruction_data);
-
-    dbg!(Instruction {
-        program_id: Pubkey::from_str("295QjveZJsZ198fYk9FTKaJLsgAWNdXKHsM6Qkb3dsVn").unwrap(),
-        accounts,
-        data: data.into_iter().flatten().collect(),
-    })
-}
 
 impl CreateMasterEdition {
     #[allow(clippy::too_many_arguments)]
@@ -85,39 +36,6 @@ impl CreateMasterEdition {
 
         Ok((minimum_balance_for_rent_exemption, instructions))
     }
-
-    #[allow(clippy::too_many_arguments)]
-    async fn command_proxy_create_metadata_accounts(
-        &self,
-        rpc_client: &RpcClient,
-        metadata_pubkey: Pubkey,
-        master_edition_pubkey: Pubkey,
-        mint: Pubkey,
-        mint_authority: Pubkey,
-        payer: Pubkey,
-        proxy_authority: Pubkey,
-        max_supply: u64,
-    ) -> crate::Result<(u64, Vec<Instruction>)> {
-        let minimum_balance_for_rent_exemption = rpc_client
-            .get_minimum_balance_for_rent_exemption(std::mem::size_of::<
-                mpl_token_metadata::state::MasterEditionV2,
-            >())
-            .await?;
-
-        let instructions = vec![create_proxy_create_master_edition_instruction(
-            &payer,
-            &proxy_authority,
-            &master_edition_pubkey,
-            &mint,
-            &mint_authority,
-            &metadata_pubkey,
-            &mpl_token_metadata::id(),
-            &spl_token::id(),
-            max_supply,
-        )];
-
-        Ok((minimum_balance_for_rent_exemption, instructions))
-    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -126,13 +44,10 @@ pub struct Input {
     pub mint_account: Pubkey,
     #[serde(with = "value::pubkey")]
     pub mint_authority: Pubkey,
-    pub is_proxy: bool,
-    #[serde(with = "value::pubkey::opt")]
-    pub proxy_authority: Option<Pubkey>,
     #[serde(with = "value::keypair")]
     pub fee_payer: Keypair,
-    #[serde(with = "value::keypair::opt")]
-    pub update_authority: Option<Keypair>,
+    #[serde(with = "value::keypair")]
+    pub update_authority: Keypair,
     pub max_supply: u64,
     #[serde(default = "value::default::bool_true")]
     pub submit: bool,
@@ -151,8 +66,6 @@ pub struct Output {
 const CREATE_MASTER_EDITION: &str = "create_master_edition";
 
 // Inputs
-const IS_PROXY: &str = "is_proxy";
-const PROXY_AUTHORITY: &str = "proxy_authority";
 const MINT_ACCOUNT: &str = "mint_account";
 const MINT_AUTHORITY: &str = "mint_authority";
 const FEE_PAYER: &str = "fee_payer";
@@ -164,22 +77,6 @@ const SUBMIT: &str = "submit";
 const SIGNATURE: &str = "signature";
 const METADATA_ACCOUNT: &str = "metadata_account";
 const MASTER_EDITION_ACCOUNT: &str = "master_edition_account";
-
-fn find_proxy_authority_address(authority: &Pubkey) -> Pubkey {
-    let (expected_pda, bump_seed) = Pubkey::find_program_address(
-        &[b"proxy", &authority.to_bytes()],
-        &Pubkey::from_str("295QjveZJsZ198fYk9FTKaJLsgAWNdXKHsM6Qkb3dsVn").unwrap(),
-    );
-
-    let actual_pda = Pubkey::create_program_address(
-        &[b"proxy", &authority.to_bytes(), &[bump_seed]],
-        &Pubkey::from_str("295QjveZJsZ198fYk9FTKaJLsgAWNdXKHsM6Qkb3dsVn").unwrap(),
-    )
-    .unwrap();
-
-    assert_eq!(expected_pda, actual_pda);
-    dbg!(actual_pda)
-}
 
 #[async_trait]
 impl CommandTrait for CreateMasterEdition {
@@ -210,7 +107,7 @@ impl CommandTrait for CreateMasterEdition {
             CmdInput {
                 name: UPDATE_AUTHORITY.into(),
                 type_bounds: [ValueType::Keypair, ValueType::String].to_vec(),
-                required: false,
+                required: true,
                 passthrough: false,
             },
             CmdInput {
@@ -224,18 +121,6 @@ impl CommandTrait for CreateMasterEdition {
                 type_bounds: [ValueType::Bool].to_vec(),
                 required: false,
                 passthrough: false,
-            },
-            CmdInput {
-                name: IS_PROXY.into(),
-                type_bounds: [ValueType::Bool].to_vec(),
-                required: true,
-                passthrough: false,
-            },
-            CmdInput {
-                name: PROXY_AUTHORITY.into(),
-                type_bounds: [ValueType::Pubkey].to_vec(),
-                required: false,
-                passthrough: true,
             },
         ]
         .to_vec()
@@ -260,8 +145,6 @@ impl CommandTrait for CreateMasterEdition {
 
     async fn run(&self, ctx: Context, inputs: ValueSet) -> Result<ValueSet, CommandError> {
         let Input {
-            is_proxy,
-            proxy_authority,
             mint_account,
             mint_authority,
             fee_payer,
@@ -275,90 +158,48 @@ impl CommandTrait for CreateMasterEdition {
         let (master_edition_account, _) =
             mpl_token_metadata::pda::find_master_edition_account(&mint_account);
 
-        if is_proxy {
-            dbg!(proxy_authority);
-            let proxy_authority = find_proxy_authority_address(&fee_payer.pubkey());
-            dbg!(proxy_authority);
-            let (minimum_balance_for_rent_exemption, instructions) = self
-                .command_proxy_create_metadata_accounts(
-                    &ctx.solana_client,
-                    metadata_account,
-                    master_edition_account,
-                    mint_account,
-                    mint_authority,
-                    fee_payer.pubkey(),
-                    proxy_authority,
-                    max_supply,
-                )
-                .await?;
-
-            let fee_payer_pubkey = fee_payer.pubkey();
-
-            let (mut transaction, recent_blockhash) = execute(
+        let (minimum_balance_for_rent_exemption, instructions) = self
+            .command_create_master_edition(
                 &ctx.solana_client,
-                &fee_payer_pubkey,
-                &instructions,
-                minimum_balance_for_rent_exemption,
+                metadata_account,
+                master_edition_account,
+                mint_account,
+                mint_authority,
+                fee_payer.pubkey(),
+                update_authority.pubkey(),
+                max_supply,
             )
             .await?;
 
-            try_sign_wallet(&ctx, &mut transaction, &[&fee_payer], recent_blockhash).await?;
+        let fee_payer_pubkey = fee_payer.pubkey();
 
-            let signature = if submit {
-                Some(submit_transaction(&ctx.solana_client, transaction).await?)
-            } else {
-                None
-            };
+        let (mut transaction, recent_blockhash) = execute(
+            &ctx.solana_client,
+            &fee_payer_pubkey,
+            &instructions,
+            minimum_balance_for_rent_exemption,
+        )
+        .await?;
 
-            Ok(value::to_map(&Output {
-                metadata_account,
-                master_edition_account,
-                signature,
-            })?)
+        try_sign_wallet(
+            &ctx,
+            &mut transaction,
+            &[&update_authority, &fee_payer],
+            recent_blockhash,
+        )
+        .await?;
+
+        let signature = if submit {
+            Some(submit_transaction(&ctx.solana_client, transaction).await?)
         } else {
-            let (minimum_balance_for_rent_exemption, instructions) = self
-                .command_create_master_edition(
-                    &ctx.solana_client,
-                    metadata_account,
-                    master_edition_account,
-                    mint_account,
-                    mint_authority,
-                    fee_payer.pubkey(),
-                    update_authority.as_ref().unwrap().pubkey(),
-                    max_supply,
-                )
-                .await?;
+            None
+        };
 
-            let fee_payer_pubkey = fee_payer.pubkey();
-
-            let (mut transaction, recent_blockhash) = execute(
-                &ctx.solana_client,
-                &fee_payer_pubkey,
-                &instructions,
-                minimum_balance_for_rent_exemption,
-            )
-            .await?;
-
-            try_sign_wallet(
-                &ctx,
-                &mut transaction,
-                &[&update_authority.unwrap(), &fee_payer],
-                recent_blockhash,
-            )
-            .await?;
-
-            let signature = if submit {
-                Some(submit_transaction(&ctx.solana_client, transaction).await?)
-            } else {
-                None
-            };
-
-            Ok(value::to_map(&Output {
-                metadata_account,
-                master_edition_account,
-                signature,
-            })?)
-        }
+        Ok(value::to_map(&Output {
+            metadata_account,
+            master_edition_account,
+            signature,
+        })?)
     }
 }
 
