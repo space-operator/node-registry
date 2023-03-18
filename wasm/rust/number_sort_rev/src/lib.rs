db573b03321bee57c3a8a68a041c3a3d0ab5b257
Knarkzel <knarkzel@gmail.com>
2023-03-18 21:44:25 +0100

    Add more

--- wasm/rust/number_sort_rev/src/lib.rs
+++ wasm/rust/number_sort_rev/src/lib.rs
@@ -1,21 +1,8 @@
-#[no_mangle]
-fn main(numbers: &String) -> Box<String> {
-    let mut numbers: Vec<i64> = serde_json::from_str(&numbers).unwrap();
-    numbers.sort();
-    numbers.reverse();
-    Box::new(serde_json::to_string(&numbers).unwrap())
-}
+use space_lib::space;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn number_sort_rev_works() {
-        let numbers: Vec<i64> = vec![50, 100000000000, -9223372036854775808, 0];
-        let numbers = serde_json::to_string(&numbers).unwrap();
-        let sorted: Vec<i64> = vec![100000000000, 50, 0, -9223372036854775808];
-        let sorted = serde_json::to_string(&sorted).unwrap();
-        assert_eq!(*main(&numbers), sorted);
-    }
+#[space]
+fn main(mut input: Vec<i64>) -> Vec<i64> {
+    input.sort();
+    input.reverse();
+    input
 }
