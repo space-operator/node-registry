0994fb13c9eefe3d863a4fdcda7984d24d1773fe
Duy Do <duydo@protonmail.com>
2023-04-04 23:12:16 +0700

    .

--- crates/cmds-solana/src/associated_token_account.rs
+++ crates/cmds-solana/src/associated_token_account.rs
@@ -2,8 +2,25 @@ use crate::prelude::*;
 use solana_program::program_pack::Pack;
 use spl_associated_token_account::instruction::create_associated_token_account;
 
-#[derive(Debug, Clone)]
-pub struct AssociatedTokenAccount;
+const SOLANA_ASSOCIATED_TOKEN_ACCOUNT: &str = "associated_token_account";
+
+const DEFINITION: &str =
+    include_str!("../../../node-definitions/solana/associated_token_account.json");
+
+fn build() -> Result<Box<dyn CommandTrait>, CommandError> {
+    use once_cell::sync::Lazy;
+    static CACHE: Lazy<Result<CmdBuilder, BuilderError>> = Lazy::new(|| {
+        CmdBuilder::new(DEFINITION)?
+            .check_name(SOLANA_ASSOCIATED_TOKEN_ACCOUNT)?
+            .simple_instruction_info("signature")
+    });
+    Ok(CACHE.clone()?.build(run))
+}
+
+inventory::submit!(CommandDescription::new(
+    SOLANA_ASSOCIATED_TOKEN_ACCOUNT,
+    |_| { build() }
+));
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Input {
@@ -23,115 +40,41 @@ pub struct Output {
     signature: Option<Signature>,
 }
 
-const SOLANA_ASSOCIATED_TOKEN_ACCOUNT: &str = "associated_token_account";
+async fn run(mut ctx: Context, input: Input) -> Result<Output, CommandError> {
+    let minimum_balance_for_rent_exemption = ctx
+        .solana_client
+        .get_minimum_balance_for_rent_exemption(spl_token::state::Account::LEN)
+        .await?;
 
-// Inputs
-const OWNER: &str = "owner";
-const FEE_PAYER: &str = "fee_payer";
-const MINT_ACCOUNT: &str = "mint_account";
-const SUBMIT: &str = "submit";
+    let instruction = create_associated_token_account(
+        &input.fee_payer.pubkey(),
+        &input.owner,
+        &input.mint_account,
+        &spl_token::id(),
+    );
 
-// Outputs
-const ASSOCIATED_TOKEN_ACCOUNT: &str = "associated_token_account";
-const SIGNATURE: &str = "signature";
+    let associated_token_account = instruction.accounts[1].pubkey;
 
-#[async_trait]
-impl CommandTrait for AssociatedTokenAccount {
-    fn instruction_info(&self) -> Option<InstructionInfo> {
-        Some(InstructionInfo::simple(self, SIGNATURE))
-    }
+    let instructions = if input.submit {
+        Instructions {
+            fee_payer: input.fee_payer.pubkey(),
+            signers: vec![input.fee_payer.clone_keypair()],
+            minimum_balance_for_rent_exemption,
+            instructions: [instruction].to_vec(),
+        }
+    } else {
+        Instructions::default()
+    };
 
-    fn name(&self) -> Name {
-        SOLANA_ASSOCIATED_TOKEN_ACCOUNT.into()
-    }
-
-    fn inputs(&self) -> Vec<CmdInput> {
-        [
-            CmdInput {
-                name: OWNER.into(),
-                type_bounds: [ValueType::Pubkey].to_vec(),
-                required: true,
-                passthrough: true,
+    let signature = ctx
+        .execute(
+            instructions,
+            value::map! {
+                "associated_token_account" => associated_token_account,
             },
-            CmdInput {
-                name: FEE_PAYER.into(),
-                type_bounds: [ValueType::Keypair].to_vec(),
-                required: true,
-                passthrough: true,
-            },
-            CmdInput {
-                name: MINT_ACCOUNT.into(),
-                type_bounds: [ValueType::Pubkey].to_vec(),
-                required: true,
-                passthrough: true,
-            },
-            CmdInput {
-                name: SUBMIT.into(),
-                type_bounds: [ValueType::Bool].to_vec(),
-                required: false,
-                passthrough: false,
-            },
-        ]
-        .to_vec()
-    }
+        )
+        .await?
+        .signature;
 
-    fn outputs(&self) -> Vec<CmdOutput> {
-        [
-            CmdOutput {
-                name: ASSOCIATED_TOKEN_ACCOUNT.into(),
-                r#type: ValueType::Pubkey,
-            },
-            CmdOutput {
-                name: SIGNATURE.into(),
-                r#type: ValueType::String,
-            },
-        ]
-        .to_vec()
-    }
-
-    async fn run(&self, mut ctx: Context, inputs: ValueSet) -> Result<ValueSet, CommandError> {
-        let input: Input = value::from_map(inputs)?;
-
-        let minimum_balance_for_rent_exemption = ctx
-            .solana_client
-            .get_minimum_balance_for_rent_exemption(spl_token::state::Account::LEN)
-            .await?;
-
-        let instruction = create_associated_token_account(
-            &input.fee_payer.pubkey(),
-            &input.owner,
-            &input.mint_account,
-            &spl_token::id(),
-        );
-
-        let associated_token_account = instruction.accounts[1].pubkey;
-
-        let instructions = if input.submit {
-            Instructions {
-                fee_payer: input.fee_payer.pubkey(),
-                signers: vec![input.fee_payer.clone_keypair()],
-                minimum_balance_for_rent_exemption,
-                instructions: [instruction].to_vec(),
-            }
-        } else {
-            Instructions::default()
-        };
-
-        let signature = ctx
-            .execute(
-                instructions,
-                value::map! {
-                    ASSOCIATED_TOKEN_ACCOUNT => associated_token_account,
-                },
-            )
-            .await?
-            .signature;
-
-        Ok(value::to_map(&Output { signature })?)
-    }
+    Ok(Output { signature })
 }
-
-inventory::submit!(CommandDescription::new(
-    SOLANA_ASSOCIATED_TOKEN_ACCOUNT,
-    |_| { Ok(Box::new(AssociatedTokenAccount)) }
-));
