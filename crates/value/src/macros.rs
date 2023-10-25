514ba81678c7e3f7ea56d9f05538ad8d7790529e
Duy Do <duydo@protonmail.com>
2023-10-25 14:23:18 +0700

    .

--- crates/value/src/macros.rs
+++ crates/value/src/macros.rs
@@ -15,3 +15,21 @@ macro_rules! map {
         }
     };
 }
+
+#[macro_export]
+macro_rules! array {
+    (@single $($x:tt)*) => (());
+    (@count $($rest:expr),*) => (<[()]>::len(&[$($crate::array!(@single $rest)),*]));
+
+    ($($value:expr,)+) => { $crate::array!($($value),+) };
+    ($($value:expr),*) => {
+        {
+            let _cap = $crate::array!(@count $($value),*);
+            let mut _vec = ::std::vec::Vec::<$crate::Value>::with_capacity(_cap);
+            $(
+                _vec.push($crate::Value::from($value));
+            )*
+            _vec
+        }
+    };
+}
